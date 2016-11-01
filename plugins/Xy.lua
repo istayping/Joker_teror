eab809f44d62e97475430e832cbed28ab38ccd55
istayping <zahra298041@gmail.com>
2016-11-01 17:41:54 +0330

    Update Xy.lua

--- plugins/Xy.lua
+++ plugins/Xy.lua
@@ -1,7 +1,8 @@
+--Coded By @dr0gh
 do
 local function run(msg, matches)
-local bot_id =  Id-Khod-bot
-local fbotmain = Id-jay
+local bot_id = -- Id-Khod-bot
+local fbotmain = msg.action.user.id
     if matches[1]:lower == 'bye' and is_admin(msg) or msg.action.type == "channel_invite" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
    --    chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
