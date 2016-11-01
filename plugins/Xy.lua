76b704b545d2321e21f7a5747f5cfacd35fcea9f
istayping <zahra298041@gmail.com>
2016-11-01 17:43:00 +0330

    Update Xy.lua

--- plugins/Xy.lua
+++ plugins/Xy.lua
@@ -6,8 +6,7 @@ local fbotmain = msg.action.user.id
     if matches[1]:lower == 'bye' and is_admin(msg) or msg.action.type == "channel_invite" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
    --    chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
-       channel_add_user("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
-	   channel_invite(channel, user, ok_cb, false)  
+       channel_invite("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
     end
 end
 return {
