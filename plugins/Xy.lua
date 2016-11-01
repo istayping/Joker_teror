b41168d9c349fd067ebc03d1f0f85ee54ed48c3f
istayping <zahra298041@gmail.com>
2016-11-01 17:07:06 +0330

    Create Xy.lua

---
+++ plugins/Xy.lua
@@ -0,0 +1,19 @@
+do
+local function run(msg, matches)
+local bot_id =  Id-Khod-bot
+local fbotmain = Id-jay
+    if matches[1]:lower == 'bye' and is_admin(msg) or msg.action.type == "channel_invite" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+   --    chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
+       channel_add_user("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
+	   channel_invite(channel, user, ok_cb, false)  
+    end
+end
+return {
+  patterns = {
+    "^([Bb]ye)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
